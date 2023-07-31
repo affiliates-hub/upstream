a3920a0ed4104e662e37cb0a4711adb0f67c79cd
ocircleo <salmanhossain11222626@gmail.com>
2023-07-31 22:12:58 +0600

    acount page code made easy

--- src/pages/acount/Acount.jsx
+++ src/pages/acount/Acount.jsx
@@ -32,54 +32,38 @@ const Acount = () => {
         <NavActiveRoutes to={'/'} toggleNav={closeNav}>welcome slide</NavActiveRoutes>
         <NavActiveRoutes to={'/'} toggleNav={closeNav}>Add controll</NavActiveRoutes>
     </>
-    const nav = <>
-        <div className='hidden sticky h-screen w-96 top-0 bg-gray-100 text-center gap-2 px-3 py-3 lg:flex items-stretch flex-col overflow-y-scroll'>
-            {
-                links
-            }
-            {
-                userRole?.role == 'admin' ? adminLink : ''
-            }
-        </div>
-
-    </>
-    const navsm = <>
-        <div className={`z-50 sticky h-screen w-full sm:w-80 top-0 bg-gray-100 text-center gap-2 px-3 py-3 flex items-stretch flex-col ${leftNav ? '-translate-x-0' : '-translate-x-80'} duration-300 overflow-y-scroll`}>
-            <div className='flex items-end justify-end text-xl h-screen overflow-y-scroll'>
-                <RxCross2></RxCross2>
-            </div>
-            {
-                links
-            }
-            {
-                userRole?.role == 'admin' ? adminLink : ''
-            }
-        </div>
-
-    </>
     return (
-        <div className='h-screen w-full flex gap-5 items-stretch relative overflow-y-hidden'>
-            {
-                nav
-            }
-            <div className={`fixed w-full duration-300 z-30   h-screen bg-gray-500/50 ${leftNav ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`} onClick={closeNav}>
+        <div className='h-screen w-full relative overflow-y-hidden flex'>
+            <div title='large screen navbar' className='h-full w-80 bg-gray-100 hidden text-center px-3 py-3 lg:flex gap-2 lg:flex-col overflow-y-scroll'>
                 {
-                    navsm
+                    links
+
+                }
+                <div className='bg-gray-300 text-gray-300 ' style={{ fontSize: '3px' }}>0</div>
+                {
+                    userRole?.role == 'admin' ? adminLink : ''
                 }
             </div>
-            <div className='w-full h-screen overflow-y-scroll'>
-                <div className='h-12 top-0 text-xl fixed lg:hidden shadow bg-white w-full flex justify-between items-center px-5' >
-                    <span className='text-xl font-semibold'>
-                        Acount
-                    </span>
-                    <span className='border-2 p-2 rounded active:scale-90 duration-200'>
-                        <GrMenu className='' onClick={toggleNav} />
-                    </span>
-                </div>
-                <div title='nav abckup ' className='h-14 w-full lg:hidden'>
-
+            <div title='main content area' className='w-full h-full relative overflow-y-scroll'>
+                <div title='toggle for small screen navbar' className='lg:hidden flex justify-between items-center text-lg font-semibold h-14 w-full sticky top-0 px-4 bg-white shadow'>
+                    <span>Acount</span>
+                    <span className='border-2 rounded p-1 active:scale-90 duration-100 cursor-pointer' onClick={toggleNav}><GrMenu className='text-xl'></GrMenu></span>
                 </div>
                 <Outlet></Outlet>
+            </div>
+            {/* Fixed items for small page  */}
+            <div title='side nav for small screen' className={`h-screen fixed z-50 w-full sm:w-80 bg-gray-100 text-center px-3 py-3 flex gap-2 flex-col overflow-y-scroll left-0 -translate-x-full duration-300 ${leftNav ? "translate-x-0" : ""}`}>
+                <div className='flex text-2xl justify-end cursor-pointer'><RxCross2 onClick={closeNav}></RxCross2></div>
+                {
+                    links
+
+                }
+                <div className='bg-gray-300 text-gray-300 ' style={{ fontSize: '3px' }}>0</div>
+                {
+                    userRole?.role == 'admin' ? adminLink : ''
+                }
+            </div>
+            <div title='nav overlay' className={`fixed w-full duration-300 z-30 h-screen bg-gray-500/50 ${leftNav ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`} onClick={closeNav}>
             </div>
         </div >
     );
